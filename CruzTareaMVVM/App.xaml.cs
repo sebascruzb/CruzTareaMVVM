6f4c73def614f1e3075beda59e4c2eb578cc376d
Raúl Sebastián Cruz Baño <raul.cruz@udla.edu.ec>
2024-06-16 19:08:28 -0500

    Agregar archivos de proyecto.

---
+++ CruzTareaMVVM/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace CruzTareaMVVM
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ CruzTareaMVVM/ViewModels/AboutViewModelsSC.cs
@@ -0,0 +1,27 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace CruzTareaMVVM.ViewModels
+{
+    internal class AboutViewModelsSC
+    {
+        public string Title => AppInfo.Name;
+        public string Version => AppInfo.VersionString;
+        public string MoreInfoUrl => "https://aka.ms/maui";
+        public string Message => "This app is written in XAML and C# with .NET MAUI.";
+        public ICommand ShowMoreInfoCommand { get; }
+
+        public AboutViewModelsSC()
+        {
+            ShowMoreInfoCommand = new AsyncRelayCommand(ShowMoreInfo);
+        }
+
+        async Task ShowMoreInfo() =>
+            await Launcher.Default.OpenAsync(MoreInfoUrl);
+    }
+}
